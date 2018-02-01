15fb52416c1d611afbe606f314e4eb38485fa5bb
Ahmed Alkabir <korabekaali@outlook.com>
2018-02-01 12:03:29 -0800

    Inital Commit

--- sbubook.py
+++ sbubook.py
@@ -1,6 +1,13 @@
 import click
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate, upgrade
 
 app = create_app('')
+migrate = Migrate(app, db)
 
 
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate the database
+    upgrade()
